3ba7f373d893c479bee49be72c2db583048cb64a
FloresPatwil <pfloresri@unsa.edu.pe>
2023-05-15 22:37:39 -0500

    Creacion de clase para test

---
+++ Laboratorio_02_Debugging/test_atm_simple_mejorado.py
@@ -0,0 +1,8 @@
+import unittest
+import atm_simple_mejorado
+class TestStringMethods(unittest.TestCase):
+    def test_deposito_cero(self):
+        # Test deposito
+        cajero = cajero()
+        cajero
+
